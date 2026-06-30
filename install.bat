e6b1bb47c3f38c33ec9a88136b702a67245cd28a
sarmendy <sarmendy@users.noreply.github.com>
2026-06-30 12:12:06 -0500

    Fix BOOTSTRAP_URL to jsdelivr

--- install.bat
+++ install.bat
@@ -7,7 +7,7 @@ cd /d "%~dp0"
 set MIN_JAVA=21
 set JDK_DIR=%LOCALAPPDATA%\jdk21
 set EXTRACT_DIR=%LOCALAPPDATA%\jdk_extract_temp
-set BOOTSTRAP_URL=https://raw.githubusercontent.com/kevinarismendy/modpack-santi/main/pack.toml
+set BOOTSTRAP_URL=https://cdn.jsdelivr.net/gh/kevinarismendy/modpack-santi@main/pack.toml
 set BOOTSTRAP_JAR=packwiz-installer-bootstrap.jar
 set PRISM_URL=https://github.com/kevinarismendy/modpack-santi/releases/download/prismlauncher-v1/PrismLauncher-Windows.zip
 set SERVER=amiguos.holy.gg
